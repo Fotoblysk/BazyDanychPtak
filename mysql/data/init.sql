17c676e27741e7b9e176d34f8664261c53125ae3
Lukasz Kaptur <lukasz.kaptur@nokia.com>
2018-05-11 03:09:01 +0200

    Added columns do db

--- mysql/data/init.sql
+++ mysql/data/init.sql
@@ -2,7 +2,7 @@ CREATE DATABASE  IF NOT EXISTS `main` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `main`;
 -- MySQL dump 10.13  Distrib 5.7.22, for Linux (x86_64)
 --
--- Host: 127.0.0.1    Database: main
+-- Host: localhost    Database: main
 -- ------------------------------------------------------
 -- Server version	5.7.22
 
@@ -27,6 +27,7 @@ DROP TABLE IF EXISTS `bills`;
 CREATE TABLE `bills` (
   `idbill` int(11) NOT NULL,
   `orders_idorders` int(11) NOT NULL,
+  `money` varchar(45) NOT NULL,
   PRIMARY KEY (`idbill`,`orders_idorders`),
   KEY `fk_bills_orders1_idx` (`orders_idorders`),
   CONSTRAINT `fk_bills_orders1` FOREIGN KEY (`orders_idorders`) REFERENCES `orders` (`idorders`) ON DELETE NO ACTION ON UPDATE NO ACTION
@@ -42,6 +43,7 @@ DROP TABLE IF EXISTS `clients`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `clients` (
   `idclients` int(11) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idclients`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -74,6 +76,9 @@ DROP TABLE IF EXISTS `credentials`;
 CREATE TABLE `credentials` (
   `idcredentials` int(11) NOT NULL,
   `employees_idemployees` int(11) NOT NULL,
+  `login` varchar(45) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password_hash` varchar(32) NOT NULL,
   PRIMARY KEY (`idcredentials`,`employees_idemployees`),
   KEY `fk_credentials_employees1_idx` (`employees_idemployees`),
   CONSTRAINT `fk_credentials_employees1` FOREIGN KEY (`employees_idemployees`) REFERENCES `employees` (`idemployees`) ON DELETE NO ACTION ON UPDATE NO ACTION
@@ -90,6 +95,9 @@ DROP TABLE IF EXISTS `deliveries`;
 CREATE TABLE `deliveries` (
   `iddeliveries` int(11) NOT NULL,
   `warehouses_idwarehouses` int(11) NOT NULL,
+  `booked` datetime NOT NULL,
+  `arrive_time_estimation` datetime NOT NULL,
+  `arrived` datetime DEFAULT NULL,
   PRIMARY KEY (`iddeliveries`,`warehouses_idwarehouses`),
   KEY `fk_deliveries_warehouses1_idx` (`warehouses_idwarehouses`),
   CONSTRAINT `fk_deliveries_warehouses1` FOREIGN KEY (`warehouses_idwarehouses`) REFERENCES `warehouses` (`idwarehouses`) ON DELETE NO ACTION ON UPDATE NO ACTION
@@ -106,6 +114,17 @@ DROP TABLE IF EXISTS `employees`;
 CREATE TABLE `employees` (
   `idemployees` int(11) NOT NULL,
   `employees_role_idemployees_role` int(11) DEFAULT NULL,
+  `name` text NOT NULL,
+  `second_name` text,
+  `surname` text NOT NULL,
+  `country` text NOT NULL,
+  `city` text NOT NULL,
+  `street` text NOT NULL,
+  `postcode` varchar(6) NOT NULL,
+  `salary` decimal(8,2) NOT NULL,
+  `hired` date NOT NULL,
+  `phone_number` varchar(12) NOT NULL,
+  `fired` date DEFAULT NULL,
   PRIMARY KEY (`idemployees`),
   KEY `fk_employees_employees_role1_idx` (`employees_role_idemployees_role`),
   CONSTRAINT `fk_employees_employees_role1` FOREIGN KEY (`employees_role_idemployees_role`) REFERENCES `employees_role` (`idemployees_role`) ON DELETE NO ACTION ON UPDATE NO ACTION
@@ -121,6 +140,7 @@ DROP TABLE IF EXISTS `employees_role`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `employees_role` (
   `idemployees_role` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
   PRIMARY KEY (`idemployees_role`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -137,6 +157,10 @@ CREATE TABLE `ingredients` (
   `deliveries_iddeliveries` int(11) DEFAULT NULL,
   `meals_idmeals` int(11) DEFAULT NULL,
   `warehouses_idwarehouses` int(11) DEFAULT NULL,
+  `name` text NOT NULL,
+  `exp_date` date DEFAULT NULL,
+  `ammount` varchar(45) DEFAULT NULL,
+  `unit` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idingredients`),
   KEY `fk_ingredients_deliveries1_idx` (`deliveries_iddeliveries`),
   KEY `fk_ingredients_meals1_idx` (`meals_idmeals`),
@@ -156,6 +180,7 @@ DROP TABLE IF EXISTS `meals`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `meals` (
   `idmeals` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
   PRIMARY KEY (`idmeals`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -169,6 +194,11 @@ DROP TABLE IF EXISTS `orders`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `orders` (
   `idorders` int(11) NOT NULL,
+  `comments` text NOT NULL,
+  `datetime1` datetime NOT NULL,
+  `status` int(1) NOT NULL,
+  `table` int(3) NOT NULL,
+  `finished_at` datetime DEFAULT NULL,
   PRIMARY KEY (`idorders`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -202,6 +232,7 @@ CREATE TABLE `orders_parts` (
   `idorders_parts` int(11) NOT NULL,
   `meals_idmeals` int(11) DEFAULT NULL,
   `employees_idemployees` int(11) NOT NULL,
+  `type` varchar(45) NOT NULL,
   PRIMARY KEY (`idorders_parts`,`employees_idemployees`),
   KEY `fk_orders_parts_meals1_idx` (`meals_idmeals`),
   KEY `fk_orders_parts_employees1_idx` (`employees_idemployees`),
@@ -222,6 +253,9 @@ CREATE TABLE `payments` (
   `clients_idclients` int(11) NOT NULL,
   `bills_idbill` int(11) NOT NULL,
   `bills_orders_idorders` int(11) NOT NULL,
+  `to_pay` decimal(6,2) NOT NULL,
+  `paid` bit(1) NOT NULL,
+  `booked` bit(1) NOT NULL,
   PRIMARY KEY (`idpayment`,`clients_idclients`,`bills_idbill`,`bills_orders_idorders`),
   KEY `fk_payments_clients1_idx` (`clients_idclients`),
   KEY `fk_payments_bills1_idx` (`bills_idbill`,`bills_orders_idorders`),
@@ -240,6 +274,7 @@ DROP TABLE IF EXISTS `rights`;
 CREATE TABLE `rights` (
   `idrights` int(11) NOT NULL,
   `credentials_idcredentials` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
   PRIMARY KEY (`idrights`,`credentials_idcredentials`),
   KEY `fk_rights_credentials1_idx` (`credentials_idcredentials`),
   CONSTRAINT `fk_rights_credentials1` FOREIGN KEY (`credentials_idcredentials`) REFERENCES `credentials` (`idcredentials`) ON DELETE NO ACTION ON UPDATE NO ACTION
@@ -256,6 +291,8 @@ DROP TABLE IF EXISTS `tockens`;
 CREATE TABLE `tockens` (
   `idtockens` int(11) NOT NULL,
   `credentials_idcredentials` int(11) NOT NULL,
+  `tocken` text NOT NULL,
+  `expires` datetime NOT NULL,
   PRIMARY KEY (`idtockens`,`credentials_idcredentials`),
   KEY `fk_tockens_credentials1_idx` (`credentials_idcredentials`),
   CONSTRAINT `fk_tockens_credentials1` FOREIGN KEY (`credentials_idcredentials`) REFERENCES `credentials` (`idcredentials`) ON DELETE NO ACTION ON UPDATE NO ACTION
@@ -271,6 +308,10 @@ DROP TABLE IF EXISTS `warehouses`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `warehouses` (
   `idwarehouses` int(11) NOT NULL,
+  `country` text NOT NULL,
+  `city` text NOT NULL,
+  `street` text NOT NULL,
+  `postcode` varchar(6) NOT NULL,
   PRIMARY KEY (`idwarehouses`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -292,4 +333,4 @@ CREATE TABLE `warehouses` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-05-11  2:17:03
+-- Dump completed on 2018-05-11  3:06:29
